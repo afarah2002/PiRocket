15de24e65174321b521d8f47a3bfd7d35d396b73
Albert Farah <arfarah2002@gmail.com>
2020-07-29 21:07:12 -0400

    live scatter plot of acceleration

--- live_data_plotter.py
+++ live_data_plotter.py
@@ -1,6 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 def read_data(file):
 	'''
@@ -19,11 +20,31 @@ def read_data(file):
 			full_data.append(instance)
 		return full_data
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+data = read_data("data/acceleration.txt")
+
+def init():
+	axes = plt.gca()
+	axes.set_xlim([-10,10])
+	axes.set_ylim([-10,10])
+	axes.set_zlim([-10,10])
+	ax.set_xlabel('X (m/s2)')
+	ax.set_ylabel('Y (m/s2)')
+	ax.set_zlabel('Z (m/s2)')
+	plt.title("3D Acceleration")
+
+	return ax
+
+def animate(i):
+	ax.scatter(data[i][0], data[i][1], data[i][2])
+
+	return ax
 
 def main():
-	accel_data = read_data("data/acceleration.txt")
-	print(accel_data)
-
+	
+	ani = FuncAnimation(fig, animate, frames=len(data), interval=50, init_func=init)
+	plt.show()
 
 
 if __name__ == '__main__':

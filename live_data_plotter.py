acaa999148d5af3442981016bb09f6a15f6f471c
Albert Farah <arfarah2002@gmail.com>
2020-07-31 16:06:54 -0400

    live acceleration plot, show only one arrow now

--- live_data_plotter.py
+++ live_data_plotter.py
@@ -3,6 +3,8 @@ from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+from arrow_generator import Arrow3D
+
 def read_data(file):
 	'''
 	Arg: txt file as str
@@ -10,6 +12,10 @@ def read_data(file):
 	File format: time is final string, makes n-sized
 				 list of n-elements before 
 	'''
+
+	xs = []
+	ys = []
+	zs = []
 
 	with open(file, 'r') as file:
 		full_data = []
@@ -21,14 +27,19 @@ def read_data(file):
 		return full_data
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = Axes3D(fig)
 data = read_data("data/acceleration.txt")
+data_transposed = np.array(data).T
+line, = ax.plot(data_transposed[0, 0:1], data_transposed[1, 0:1], data_transposed[2, 0:1])
+
+
+
 
 def init():
 	axes = plt.gca()
-	axes.set_xlim([-10,10])
-	axes.set_ylim([-10,10])
-	axes.set_zlim([-10,10])
+	axes.set_xlim3d([-10,10])
+	axes.set_ylim3d([-10,10])
+	axes.set_zlim3d([-10,10])
 	ax.set_xlabel('X (m/s2)')
 	ax.set_ylabel('Y (m/s2)')
 	ax.set_zlabel('Z (m/s2)')
@@ -36,14 +47,31 @@ def init():
 
 	return ax
 
-def animate(i):
-	ax.scatter(data[i][0], data[i][1], data[i][2])
+def update(i, data, line):
+	# use for position 
+	# print(data[:2, :i])
+	# print("")
+	# line.set_data(data[:2, :i])
+	# line.set_3d_properties(data[2, :i]) 
+	ax.clear()
+	# must setup the plot again now
+	axes = plt.gca()
+	axes.set_xlim3d([-10,10])
+	axes.set_ylim3d([-10,10])
+	axes.set_zlim3d([-10,10])
+	ax.set_xlabel('X (m/s2)')
+	ax.set_ylabel('Y (m/s2)')
+	ax.set_zlabel('Z (m/s2)')
+	plt.title("3D Acceleration")
+	a = Arrow3D([0, data[i][0]], [0, data[i][1]], [0, data[i][2]], mutation_scale=20, lw=1, arrowstyle="-|>", color="r")
+	ax.add_artist(a)
 
-	return ax
+	return line
 
 def main():
 	
-	ani = FuncAnimation(fig, animate, frames=len(data), interval=50, init_func=init)
+	ani = FuncAnimation(fig, update, len(data_transposed[0]), interval=10000/69, init_func=init, fargs=(data_transposed, line))
+	ani = FuncAnimation(fig, update, len(data), interval=50, init_func=init, fargs=(data, line))
 	plt.show()
 
 

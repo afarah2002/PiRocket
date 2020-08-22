84b2bb6d59f82832591b93a6af5a7a356396b1df
Albert Farah <arfarah2002@gmail.com>
2020-08-21 22:11:16 -0400

    i think i fixed the local --> global thing, i think recognizes gravity now, had to reverse yaw

--- live_data_plotter.py
+++ live_data_plotter.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import pyglet
+from pyglet.gl import *
 from pytransform3d.plot_utils import Frame
 from pytransform3d.rotations import *
-import numpy as np
+import ray
 from scipy import integrate as it
+from threading import Thread
+import multiprocessing
+import time
 
 from arrow_generator import Arrow3D
 
@@ -68,14 +74,25 @@ def calibrate(data_T): # WILL WORK PREMATURELY ONLY WITH ANGVEL
 
 	return calibratedData
 
-accel_data = read_data("data/acceleration.txt")
+# most recent test
+acc_file_new = "data/acceleration.txt"
+angvel_file_new = "data/attitude.txt"
+
+# test 8
+test_num = 8
+acc_file_old = "old_data/test" + str(test_num) + "/acceleration.txt"
+angvel_file_old = "old_data/test" + str(test_num) + "/attitude.txt"
+
+
+accel_data = read_data(acc_file_new)
 print("Number of points:", len(accel_data))
-accelDataTransposed = np.array(accel_data).T
+accelDataTransposed = np.multiply(accel_data, np.array([-1,-1,-1,1])).T
+# print(np.array(accel_data).T)
 accelCalibrated = calibrate(accel_data)
 velocityDataTransposed, positionDataTransposed = integrate(accelDataTransposed) 
 
-angvelData = read_data("data/attitude.txt")
-angvelDataTransposed = np.array(angvelData).T
+angvelData = read_data(angvel_file_new)
+angvelDataTransposed = np.array(np.multiply(angvelData, np.array([1,1,-1,1]))).T
 angvelCalibrated = calibrate(angvelDataTransposed)
 orientationData = np.array(integrate(angvelCalibrated)[0]) # [0] index is to get the first result returned
 
@@ -90,7 +107,12 @@ ax4 = fig.add_subplot(223) # acceleration, noise
 ax5 = fig.add_subplot(224) # angular velocity, noise
 
 
-def update(i, acceleration, position, orientation, frame):
+# l, v = ax4.plot(0, -11, accel_data[-1][-1]+1, 11, linewidth=2, color= 'black')
+l4 = ax4.axvline(x=0., color='k')
+l5 = ax5.axvline(x=0., color='k')
+
+
+def update(i, acceleration, position, orientation, frame, line4, line5):
 
 	# ax3 = orientation 
 	ax3.set_xlim((-1, 1))
@@ -102,8 +124,8 @@ def update(i, acceleration, position, orientation, frame):
 	ax3.set_title("Orientation")
 
 	roll = orientation[i][0]
-	pitch = orientation[i][1]
-	yaw = orientation[i][2]
+	pitch = orientation[i][1] 
+	yaw = orientation[i][2] 
 	euler = [roll, pitch, yaw]
 	R = matrix_from_euler_xyz(euler)
 	A2B = np.eye(4)
@@ -120,11 +142,13 @@ def update(i, acceleration, position, orientation, frame):
 	ax1.set_zlabel('Z (m/s2)')
 	ax1.set_title("3D Acceleration")
 
-	acc_array = np.multiply(np.array(acceleration[i][0:3]), np.array([1,1,1]))
+	# acc_array = np.multiply(np.array(acceleration[i][0:3]), np.array([1,1,1]))
+	acc_array = acceleration[i][0:3]
 
 	acc_prime = np.dot(acc_array, R)
 	# print(np.array(acceleration[i][0:3]))
-	print(acc_prime)
+	print("Original: ", np.round(acc_array, 3), "		", "Primed", np.round(acc_prime, 3))
+
 	acc_x = acc_prime[0]
 	acc_y = acc_prime[1]
 	acc_z = acc_prime[2]
@@ -149,7 +173,12 @@ def update(i, acceleration, position, orientation, frame):
 	pos_z = pos_prime[2]
 	ax2.scatter(pos_x, pos_y, pos_z)
 
-	return frame
+	t = acceleration[i][3]
+
+	line4.set_data([t,t], [-11,11])
+	line5.set_data([t,t], [-11,11])
+
+	return frame, line4, line5,
 
 
 def noise(acc, ang_vel):
@@ -157,10 +186,13 @@ def noise(acc, ang_vel):
 	# fig, (ax1, ax2) = plt.subplots(2)
 	# fig.suptitle('Noise analysis')
 	# each plot plots all three axes
+	# ax4.clear()
 	ax4.set_title('3-Axis Accelerometer')
 	ax4.plot(acc[3],acc[0],color='red')
-	ax4.plot(acc[3],-acc[1],color='green')
+	ax4.plot(acc[3],acc[1],color='green')
 	ax4.plot(acc[3],acc[2],color='blue')
+	ax4.set_xlim([0,accel_data[-1][-1]+1])
+	ax4.set_ylim([-11,11])
 	ax4.set_xlabel('Time (s)')
 	ax4.set_ylabel('Acceleration (m/s2)')
 
@@ -173,7 +205,10 @@ def noise(acc, ang_vel):
 
 	# plt.show()
 
-	
+
+# ray.init()
+
+# @ray.remote
 def flight_plotter():
 
 	# print(accelCalibrated)
@@ -184,12 +219,75 @@ def flight_plotter():
 	frame.add_frame(ax3)
 
 	ani = FuncAnimation(
-		fig, update, len(accel_data), interval=25, fargs=(accel_data, positionDataTransposed, orientationData, frame), blit=False)
+		fig, update, len(accel_data), interval=150, fargs=(accel_data, positionDataTransposed, orientationData, frame, l4, l5), blit=False)
 	plt.show()
-
 
 if __name__ == '__main__':
 	flight_plotter()
+
+'''
+window = pyglet.window.Window(width=1440, height=960)
+window.projection = pyglet.window.Projection3D()
+batch = pyglet.graphics.Batch()
+
+@window.event
+def on_draw():
+	window.clear()
+	batch.draw()
+
+
+@window.event
+def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+	glRotatef(1, dx, dy, 0)
+
+def rotate_model(dt, dx, dy, dz):
+	# rotate around x
+	glRotatef(dx, 1, 0, 0)
+	# rotate around y
+	glRotatef(dy, 0, 1, 0)
+	# rotate around z
+	glRotatef(dz, 0, 0, 1)
+
+
+glEnable(GL_MULTISAMPLE_ARB)
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
+
+model = pyglet.model.load("rocket.obj", batch=batch)
+glTranslatef(0, 0, -3)
+# print(len(orientationData))
+# @ray.remote
+def run():
+	for i in orientationData:
+		dx = i[0]
+		dy = i[1]
+		dz = i[2]
+		# rotate_model(0, dx, dy, dz)
+		print(i)
+		pyglet.clock.schedule_interval(rotate_model, 1/120, dx, dy, dz)
+		
+		pyglet.app.run()
+
+'''
+# if __name__ == '__main__':
+# 	flight_plotter()
+	# run()
+	# modeller()
+	# p1 = multiprocessing.Process(target=flight_plotter)
+	# p2 = multiprocessing.Process(target=run)
+
+	# p1.start()
+	# p2.start()
+	# Thread(target = flight_plotter).start()
+	# Thread(target = modeller).start()
+
+	# p1.join()
+	# p2.join()
+	# flight_plotter()
+
+	# modeller()
+	# ray.get([flight_plotter.remote(), run.remote()])
 
 
 
